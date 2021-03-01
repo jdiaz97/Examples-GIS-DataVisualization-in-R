a986378ae096d5c38a9bd761659fc4429c3fb6c5
FinchoDM <jose.diaz1601@alumnos.ubiobio.cl>
2021-03-01 16:17:14 -0300

    update

--- Eg3 Scatterplots/Eg3.1/Scatterplot2ForLoops.R
+++ Eg3 Scatterplots/Eg3.1/Scatterplot2ForLoops.R
@@ -6,14 +6,20 @@ df <- import("1.xlsx")
 for (j in unique(df$Evaluación)){
   for (i in unique(df$Especie)){
     a <- ggplot(df[df$Especie == i & df$Evaluación == j,], aes(x = Fecha, y = `Tamaño (cm)`, 
-                                          color = Tratamiento, group = Tratamiento)) + 
-      guides(color = guide_legend(reverse = TRUE)) +
-      geom_point() + 
+                                                               color = Tratamiento, 
+                                                               group = Tratamiento, shape = Tratamiento)) + 
+      guides(color = guide_legend(reverse = TRUE), shape = guide_legend(reverse = TRUE)) +
+      geom_point(aes(shape = Tratamiento)) + 
       geom_line() +
       theme_classic() +
       ylab(j) +
-      ggtitle(i)
+      ggtitle(i) +
+      scale_shape_manual(values = shapes) +
+      scale_color_manual(values = cols)
     print(a)
     ggsave(filename = paste(i, j, ".png"))
   }
 }
+
+
+  
