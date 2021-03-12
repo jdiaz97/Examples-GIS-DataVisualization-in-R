4fba7ff2a5980c48053b856a6eb3cf7b6fd6f0a0
FinchoDM <jose.diaz1601@alumnos.ubiobio.cl>
2021-03-12 03:02:00 -0300

    upÂdate

--- Eg3 Scatterplots/Eg3.1/Scatterplot2ForLoops.R
+++ Eg3 Scatterplots/Eg3.1/Scatterplot2ForLoops.R
@@ -1,9 +1,6 @@
 library(rio)
 library(ggplot2)
 library(reshape2)
-library(extrafont)
-font_import()
-loadfonts(device = "win")
 
 df <- import("1.xlsx")
 
@@ -16,12 +13,14 @@ for (j in unique(df$Evaluación)){
   for (i in unique(df$Especie)){
     a <- ggplot(df[df$Especie == i & df$Evaluación == j,], aes(x = Fecha, y = `Tamaño (cm)`, 
                                                                color = Tratamiento, 
-                                                               group = Tratamiento, shape = Tratamiento)) + 
+                                                               group = Tratamiento, 
+                                                               shape = Tratamiento)) + 
       guides(color = guide_legend(reverse = TRUE), shape = guide_legend(reverse = TRUE)) +
       geom_point() + 
       geom_line() +
       theme_classic() +
       ylab(j) +
+      xlab("Año") +
       ggtitle(i) +
       scale_shape_manual(values = shapes) +
       scale_color_manual(values = cols)
@@ -43,6 +42,7 @@ for (j in unique(df$Evaluación)){
       geom_line() +
       theme_classic() +
       ylab(j) +
+      xlab("Año") +
       ggtitle(i) +
       scale_color_manual(values = cols) +
       scale_shape_identity()
@@ -60,10 +60,11 @@ for (j in unique(df$Evaluación)){
       guides(color = guide_legend(reverse = TRUE), shape = guide_legend(reverse = TRUE)) +
       geom_point(size = 0, show.legend = FALSE) + 
       geom_text(
-        aes(label = Tukey, stroke = 4), size = 8, show.legend = FALSE) +
+        aes(label = Tukey), size = 8, show.legend = FALSE) +
       geom_line() +
       theme_classic() +
       ylab(j) +
+      xlab("Año") +
       ggtitle(i) +
       scale_color_manual(values = cols) +
       scale_shape_identity()
