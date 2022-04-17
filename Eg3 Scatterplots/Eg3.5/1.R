8e6bb00362a2587f774ac6ce40a3dba1b3253b0f
jdiaz97 <jose.diaz1601@alumnos.ubiobio.cl>
2022-04-17 02:28:46 -0400

    refactored

--- Eg3 Scatterplots/Eg3.5/1.R
+++ Eg3 Scatterplots/Eg3.5/1.R
@@ -19,19 +19,27 @@ finaldf$value <- factor(finaldf$value, levels = c("Diciembre", "Enero", "Febrero
                                                   "Mayo", "Junio", "Julio", "Agosto", "Septiembre", "Octubre",
                                                   "Noviembre"))
 
-finaldf$Nombre.completo <- paste(df$`Nombre científico`,"-",df$`Nombre común`)
+finaldf$Nombre.completo <- paste(df$`Nombre común`,"-",df$`Nombre científico`)
 
-ggplot(finaldf) + aes(x = value, y = Nombre.común, color = Nombre.común) +
-  geom_point(size = 4.5) +
-  xlab("") +
-  ylab("") +
-  scale_y_discrete(limits = rev) +
-  theme_light() +
-  theme(legend.position = "none",text=element_text(family="serif", size = 13, face = "bold"),
-        plot.title = element_text(hjust = 0.5, size = 20)) +
-  ggtitle("Fechas de recolección de semillas") +
-  geom_text(aes(label = Autor, family = "serif", size = 12), x = 14.5) +
-  coord_cartesian(xlim = c(0.9, 16), # This focuses the x-axis on the range of interest
-                  clip = 'off')    # This keeps the labels from disappearing
+plotting <- function(input, xx){
+  ggplot(input) + aes(x = value, y = .data[[xx]], color = .data[[xx]]) +
+    geom_point(size = 4.5) +
+    xlab("") +
+    ylab("") +
+    scale_y_discrete(limits = rev) +
+    theme_light() +
+    theme(legend.position = "none",text=element_text(family="serif", size = 13, face = "bold"),
+          plot.title = element_text(hjust = 0.5, size = 20)) +
+    ggtitle("Fechas de recolección de semillas") +
+    geom_text(aes(label = Autor, family = "serif", size = 12), x = 14.5) +
+    labs(caption = "@viveroelmono") +
+    theme(plot.caption = element_text(hjust = 1, vjust = 15)) +
+    coord_cartesian(xlim = c(0.9, 16), # This focuses the x-axis on the range of interest
+                    clip = 'off')    # This keeps the labels from disappearing
+}
 
-ggsave("1.jpg", width = 13.9, height = 9)
+a <- plotting(finaldf,"Nombre.común")
+ggsave("1.jpg",a, width = 13.9, height = 9)
+b <- plotting(finaldf,"Nombre.completo")
+ggsave("2.jpg",b, width = 15.1, height = 9)
+
